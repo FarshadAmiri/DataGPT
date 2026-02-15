3fb45c4ddd799caa30495669067c5f60261b3b45
Farshad Amiri <47373990+FarshadAmiri@users.noreply.github.com>
2026-02-15 15:08:12 +0330

    excessive files removed

--- start_vllm.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-wsl -e bash -ic "cd ~ && source vllm-venv/bin/activate && python -m vllm.entrypoints.openai.api_server --host 0.0.0.0 --port 8002 --model /home/farshad/models/Qwen3-4B-AWQ --served-model-name Qwen3-4B --dtype auto --quantization awq --max-model-len 16384 --gpu-memory-utilization 0.80 --enforce-eager --swap-space 1; exec bash"
